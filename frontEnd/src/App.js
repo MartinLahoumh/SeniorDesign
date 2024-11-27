67676825fcbe0a359e5326b35eec0b79907e1408
MartinLahoumh <martinlahoumh103102@gmail.com>
2024-11-27 12:21:31 -0500

    design changes

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -5,13 +5,17 @@ import Translate from './components/Translate';
 import './styles/App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
+
   return (
     <div className='app-body'>
       <div className='app-content'>
         <header className="app-header">
-          <div className='logo'>
-            <h1 className='logo-name'>American Dream</h1>
-            <img className='logo-img'src={logo}/>
+          <div className='header-content'>
+            <div className='logo'>
+              <h1 className='logo-name'>American Dream</h1>
+              <img className='logo-img'src={logo}/>
+            </div>
+            <p >Log In</p>
           </div>
         </header>
         <Router>
